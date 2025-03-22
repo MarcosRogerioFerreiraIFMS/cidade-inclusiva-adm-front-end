bc4dbf14cd730182f87785d2d9921de2a80b0605
Kevin Souza <kevinwillyansouza@gmail.com>
2025-03-21 21:07:33 -0300

    feat: adicionar seção de Acesso Rápido com ações para listar e adicionar notícias

--- src/app/noticias/page.tsx
+++ src/app/noticias/page.tsx
@@ -1,9 +1,29 @@
 import { Container } from '../_components/Container'
+import { QuickActionCard } from '../_components/QuickActionCard'
+
+const QuickActions = [
+  {
+    title: 'Listar Notícias',
+    description: 'Visualize uma tabela com todas as notícias',
+    href: '/noticias/listar'
+  },
+  {
+    title: 'Adicionar Notícia',
+    description: 'Adicione uma nova notícia',
+    href: '/noticias/adicionar'
+  }
+]
 
 export default function Noticias() {
   return (
     <Container>
-      <h1 className="text-4xl text-slate-900">Notícias</h1>
+      <h2 className="text-2xl">Acesso Rápido</h2>
+
+      <section className="grid grid-cols-1 gap-5 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+        {QuickActions.map((action, index) => (
+          <QuickActionCard key={index} {...action} />
+        ))}
+      </section>
     </Container>
   )
 }
